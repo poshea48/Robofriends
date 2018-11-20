04c3d902f31268528861bd2eeafbd78f9feaa6b6
Paul O'Shea <poshea48@msn.com>
2018-11-19 14:21:32 -1000

    updated label in searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,14 +5,16 @@ const SearchBox = ({searchChange}) => {
   console.log("SearchBox")
   return (
     <div className="pa2">
-      <label for="searchfield" ></label>
-      <input
-        className="pa3 ba b--green bg-lightest-blue'"
-        name="searchfield"
-        type="search"
-        placeholder="search robots"
-        onChange={searchChange}
-      />
+      <label for="searchfield" aria-label aria-labelledby >
+        <input
+          id="searchfield"
+          className="pa3 ba b--green bg-lightest-blue'"
+          name="searchfield"
+          type="search"
+          placeholder="search robots"
+          onChange={searchChange}
+        />
+      </label>
     </div>
   )
 }
